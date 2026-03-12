f1663d5bde60e89dda0c6c0f2041143b5ba4ec54
pawatkriti-hash <pawat.kriti@bumail.net>
2026-03-12 10:30:44 +0700

    Script Torgue

--- Assets/Script/TorqueRotate.cs
+++ Assets/Script/TorqueRotate.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TorqueRotate : MonoBehaviour
 {
+    public float angularSpeed;
+
+    Rigidbody rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Keyboard.current.dKey.isPressed)
+        {
+            rb.angularVelocity = new Vector3(0, 0, angularSpeed);
+        }
+        else
+
+        {
+
+            rb.angularVelocity = Vector3.zero;
+
+        }
+
     }
+
 }
