165806e50cb92e228977be798955960ecbe19037
pawatkriti-hash <pawat.kriti@bumail.net>
2026-03-12 10:45:38 +0700

    Script Magnus Kick

--- Assets/Script/MagnusEffectKlck.cs
+++ Assets/Script/MagnusEffectKlck.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class MagnusEffectKlck : MonoBehaviour
 {
+    public float KickForce;
+    public float SpinAmount;
+    public float MagnusStrength = 0.5f;
+
+    Rigidbody rb;
+    bool isShot = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && !isShot)
+        {
+            rb.AddForce(Vector3.forward * KickForce, ForceMode.Impulse);
+            
+            rb.AddTorque(Vector3.up * SpinAmount);
+            
+            isShot = true;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (isShot) return;
+
+        Vector3 velocity = rb.linearVelocity;
+        Vector3 spin = rb.angularVelocity;
+
+        Vector3 magnusForce = MagnusStrength * Vector3.Cross(spin, velocity);
+
+        rb.AddForce(magnusForce);
     }
 }
